1dd447455871486970f3e95890701985ca5e94ce
Sam <74011113+crankyMagician@users.noreply.github.com>
2023-12-21 22:19:55 -0500

    Update added in bootstrap, remotely connecting to devices
    Update added in bootstrap, remotely connecting to devices

--- bluetooth-scanner/src/App.js
+++ bluetooth-scanner/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BluetoothScanner from './components/BluetoothScanner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- bluetooth-scanner/src/components/BluetoothScanner.js
+++ bluetooth-scanner/src/components/BluetoothScanner.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { knownServices } from '../consts/KnownServices';
 import { knownCharacteristics } from '../consts/KnownCharacteristics';
 
-
 const BluetoothDeviceScanner = () => {
     const [devices, setDevices] = useState([]);
     const [isScanning, setIsScanning] = useState(false);
@@ -14,21 +13,47 @@ const BluetoothDeviceScanner = () => {
         setDevices(prevDevices => [...prevDevices, { device, services }]);
     };
 
+    const disconnectDevice = (deviceIndex) => {
+        const device = devices[deviceIndex].device;
+        if (device.gatt.connected) {
+            device.gatt.disconnect();
+            console.log(`Disconnected from ${device.name}`);
+        }
+        setDevices(prevDevices => prevDevices.filter((_, index) => index !== deviceIndex));
+    };
+
     const scanDevices = async () => {
         setIsScanning(true);
         try {
             const options = {
                 acceptAllDevices: true,
-                optionalServices: Object.keys(knownServices), // Use all known services
-                optionalCharacteristics: Object.keys(knownCharacteristics) // Use all known characteristics
+                optionalServices: Object.keys(knownServices).length > 0 ? Object.keys(knownServices) : undefined
             };
 
             const device = await navigator.bluetooth.requestDevice(options);
             const server = await device.gatt.connect();
-            const services = await server.getPrimaryServices();
+
+            device.addEventListener('gattserverdisconnected', () => {
+                console.log(`Device ${device.name} got disconnected. Trying to reconnect...`);
+                reconnectDevice(device);
+            });
+
+            let services;
+            try {
+                services = await server.getPrimaryServices();
+            } catch (serviceError) {
+                console.error('Error fetching services:', serviceError);
+                services = []; // Proceed with an empty array if no services are found
+            }
 
             const servicesWithCharacteristics = await Promise.all(services.map(async service => {
-                const characteristics = await service.getCharacteristics();
+                let characteristics;
+                try {
+                    characteristics = await service.getCharacteristics();
+                } catch (charError) {
+                    console.error('Error fetching characteristics:', charError);
+                    characteristics = []; // Proceed with an empty array if no characteristics are found
+                }
                 return {
                     service: getServiceName(service.uuid),
                     characteristics: characteristics.map(c => ({
@@ -46,23 +71,39 @@ const BluetoothDeviceScanner = () => {
         }
     };
 
+    const reconnectDevice = async (device) => {
+        try {
+            await device.gatt.connect();
+            console.log(`Reconnected to ${device.name}`);
+        } catch (reconnectError) {
+            console.error(`Failed to reconnect to ${device.name}:`, reconnectError);
+        }
+    };
+
+
     return (
-        <div>
-            <button onClick={scanDevices} disabled={isScanning}>
+        <div className="container my-4">
+            <button className="btn btn-primary mb-3" onClick={scanDevices} disabled={isScanning}>
                 {isScanning ? 'Scanning...' : 'Scan for Bluetooth Devices'}
             </button>
-            <ul>
+            <ul className="list-group">
                 {devices.map((item, index) => (
-                    <li key={index}>
-                        {item.device.name || 'Unnamed Device'}
-                        <ul>
+                    <li key={index} className="list-group-item">
+                        <strong>{item.device.name || 'Unnamed Device'}</strong>
+                        <button
+                            className="btn btn-danger btn-sm float-right"
+                            onClick={() => disconnectDevice(index)}
+                        >
+                            Disconnect
+                        </button>
+                        <ul className="list-group list-group-flush">
                             {item.services.map((service, serviceIndex) => (
-                                <li key={serviceIndex}>
-                                    Service: {service.service}
-                                    <ul>
+                                <li key={serviceIndex} className="list-group-item">
+                                    <span className="text-primary">Service:</span> {service.service}
+                                    <ul className="list-group">
                                         {service.characteristics.map((characteristic, charIndex) => (
-                                            <li key={charIndex}>
-                                                Characteristic: {characteristic.name}
+                                            <li key={charIndex} className="list-group-item">
+                                                <span className="text-success">Characteristic:</span> {characteristic.name}
                                             </li>
                                         ))}
                                     </ul>
